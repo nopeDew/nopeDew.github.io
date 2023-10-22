9da01a1ac588bc053e73049699d23bd7527beda1
defaultpilot <115365312+defaultpilot@users.noreply.github.com>
2023-10-22 09:42:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var object2 = document.getElementById("object1")[0];
-var object2 = document.getElementById("object3")[0];
+var object3 = document.getElementById("object3")[0];
+var object4 = document.getElementById("object4")[0];
 function overlap (element1, element2) {
     var eva = element1.getBoundingClientRect();
     var evagelion = element2.getBoudingClientRect();
