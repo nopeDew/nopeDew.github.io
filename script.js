ea52620f32d2558f805bddf00bcbe672105a8f51
defaultpilot <115365312+defaultpilot@users.noreply.github.com>
2023-10-23 16:36:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 addEventListener("animationend", (event) => {
   if (event.animationName == "moveleft") {
+    var object1 = document.getElementById("object1");
+    var object2 = document.getElementById("object2");
     document.body.style.backgroundImage = "url('https://giffiles.alphacoders.com/113/113020.gif')";
-    document.body.object1.style.color = 'transparent';
-    document.body.object1.style.backgroundImage = "url('https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1')";
-    document.body.object2.style.color = 'transparent';
-    document.body.object2.style.backgroundImage = "url('https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1')";
+    object1.style.opacity = '0';
+    object1.style.backgroundImage = "url('https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1')";
+    object2.style.opacity = '0';
+    object2.style.backgroundImage = "url('https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1')";
   }
 });
