554a7c8ab24b1513aa452f340585a2367711976b
defaultpilot <115365312+defaultpilot@users.noreply.github.com>
2023-10-22 21:25:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-var object3 = document.getElementById("object3")[0];
-var object4 = document.getElementById("object4")[0];
-function overlap (element1, element2) {
-    var eva = element1.getBoundingClientRect();
-    var evangelion = element2.getBoudingClientRect();
-    var overlap = !(eva.right < evangelion.left || eva.left > evangelion.right || eva.bottom < evangelion.top || eva.top > evangelion.bottom);
-    if (overlap == false) {
-        document.body.style.backgroundImage = "url('https://giffiles.alphacoders.com/113/113020.gif')";
-    }
-}
-setTimeout(overlap, 1500, object3, object4);
-
+addEventListener("animationend", (event) => {
+  if (event.animationName == "moveleft") {
+    document.body.style.backgroundImage = "url('https://giffiles.alphacoders.com/113/113020.gif')";
+  }
+});
