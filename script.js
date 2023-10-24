c5ac61e5275de897793ad6e1b2db08a5f3e9f3da
defaultpilot <115365312+defaultpilot@users.noreply.github.com>
2023-10-24 18:29:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 addEventListener("animationend", (event) => {
-  if (event.animationName == "moveleft") {
+  if (event.animationName == "moveright") {
     document.body.style.backgroundImage = "url('https://giffiles.alphacoders.com/113/113020.gif')";
     document.body.object1.style.backgroundImage = "url('https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1')";
     document.body.object2.style.backgroundImage = "url('https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1')";
